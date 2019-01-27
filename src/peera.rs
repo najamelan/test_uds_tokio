dd778d837802954ebb016cf28be166417c3eb044
Naja Melan <najamelan@autistici.org>
2019-01-27 02:33:15 +0100

    Works with new style async

--- src/peera.rs
+++ src/peera.rs
@@ -1,3 +1,15 @@
+// https://jsdw.me/posts/rust-asyncawait-preview/
+
+
+// enable the await! macro, async support, and the new std::Futures api.
+//
+#![feature(await_macro, async_await, futures_api)]
+
+// only needed to manually implement a std future:
+//
+#![feature(arbitrary_self_types)]
+
+
 // 1. Spawn peerB and later also peerC
 // 2. Give peerB our socket address
 // 3. listen for a connection
@@ -6,9 +18,11 @@
 
 
 use tokio::prelude::*;
-use tokio_uds::UnixListener;
+// use futures::prelude::*;
+use tokio_uds::{ UnixListener, UnixStream };
 use std::process::Command;
 use std::str;
+// use failure::Error;
 
 
 fn main()
@@ -27,54 +41,68 @@ fn main()
 	std::fs::remove_file( SOCK_ADDR ).expect( "PeerA: Could remove socket file" );
 	let listener = UnixListener::bind( &SOCK_ADDR ).expect( "PeerA: Could not bind to socket" );
 
+	tokio::run_async
+	(
+		async
+		{
+			let mut incoming = listener.incoming();
 
-	let server = listener.incoming().for_each( |socket| -> tokio::io::Result<()>
-	{
-		println!( "PeerA: Start server future" );
-
-		let (reader, _writer) = socket.split();
-
-
-		let receive = tokio::io::read_to_end( reader, vec![] ).and_then( |(_readhalf, buf)|
-
+			while let Some( stream ) = await!( incoming.next() )
 			{
-				tokio::spawn( process( str::from_utf8( &buf ).expect( "Invalid utf encoding received" ) ) );
+				println!("PeerA: New stream!");
 
-				Ok(())
-			})
-
-			.map_err( |error|
-			{
-				eprintln!( "PeerA: Error while reading from socket: {:?}", error );
-			});
-
-
-		tokio::spawn( receive );
-
-		Ok(())
-	})
-
-	.map_err( |err|
-	{
-		// Handle error by printing to STDOUT.
-		//
-		eprintln!( "PeerA: accept error = {:?}", err );
-	});
-
-	println!( "PeerA: server running on {}", SOCK_ADDR );
-
-
-	tokio::run( server );
+				tokio::spawn_async( handle_stream( stream.expect( "PeerA: Invalid Stream" ) ) );
+			}
+		}
+	);
 }
 
 
-fn process( message: &str ) -> future::FutureResult<(),()>
+// TODO: Bubble up errors
+//
+async fn handle_stream( mut stream: UnixStream )
+{
+	let mut out: Vec<u8> = Vec::new();
+	let mut buf          = [0u8; 1024];
+
+
+	let success: bool = loop
+	{
+		let read = await!( stream.read_async( &mut buf ) );
+
+		if let Ok( amount ) = read
+		{
+			if amount == 0 { break true; }
+
+			println!( "PeerA: Read {:?} bytes", amount );
+			out.extend_from_slice( &buf[ 0..amount ] );
+
+			continue;
+		}
+
+		else if let Err( err ) = read
+		{
+			eprintln!( "PeerA: Error during reading from stream: {:?}", err );
+
+			break false;
+		}
+	};
+
+	if success
+	{
+		tokio::spawn_async
+		(
+			process( str::from_utf8( &out ).expect( "PeerA: Got invalid utf from stream" ).to_string() )
+		);
+	}
+}
+
+
+async fn process( message: String )
 {
 	println!( "PeerA: process called" );
 
-	println!("PeerA: received: {:?}", message );
-
-	future::ok(())
+	println!( "PeerA: received: {:?}", &message );
 }
 
 
--- src/peerb.rs
+++ src/peerb.rs
@@ -1,8 +1,18 @@
+
+// enable the await! macro, async support, and the new std::Futures api.
+//
+#![feature(await_macro, async_await, futures_api)]
+
+// only needed to manually implement a std future:
+//
+#![feature(arbitrary_self_types)]
+
 use std::env::args;
 
 use tokio::prelude::*;
 use tokio::io;
 use tokio_uds::UnixStream;
+use tokio_async_await::compat::forward::IntoAwaitable;
 
 fn main()
 {
@@ -17,31 +27,31 @@ fn main()
 
 	println!( "PeerB: socket addres set to: {:?}", sock_addr );
 
-	let connection = UnixStream::connect( &sock_addr );
-
-
-	let writing = connection.and_then( |socket|
+	let writing = async
 	{
+		let mut socket = await! ( UnixStream::connect( sock_addr ).into_awaitable() ).expect( "PeerB: Failed to connect" );
+
 		println!( "PeerB: start writing future" );
 
+		match await!( socket.write_async( "PeerB says hi.".as_bytes() ) )
+		// match await!( io::write_all( socket, "PeerB says hi." ).into_awaitable() )
+		{
+			Ok (_) => { println! ( "PeerB: successfully wrote to stream"       ); },
+			Err(e) => { eprintln!( "PeerB: failed to write to stream: {:?}", e ); }
+		}
 
-		io::write_all( socket, "PeerB says hi.")
+		std::thread::sleep_ms( 5000 );
 
-			.then( |result|
-			{
-				match result
-				{
-					Ok(_) => { println!( "PeerB: successfully wrote to stream" ); },
-					Err(e) => { eprintln!("PeerB: failed to write to stream: {:?}", e ); }
-				}
+		match await!( socket.write_async( "PeerB says hi.".as_bytes() ) )
+		// match await!( io::write_all( socket, "PeerB says hi." ).into_awaitable() )
+		{
+			Ok (_) => { println! ( "PeerB: successfully wrote to stream"       ); },
+			Err(e) => { eprintln!( "PeerB: failed to write to stream: {:?}", e ); }
+		}
+	};
 
-				Ok(())
-			})
-	})
-		.map    ( |_| () )
+	tokio::run_async( writing );
 
-		.map_err( |e| { eprintln!( "PeerB: Writing to stream failed: {:?}", e ); } );
-
-
-	tokio::run( writing );
+	println!( "PeerB: Shutting down" );
 }
+
